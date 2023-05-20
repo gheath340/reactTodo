09391e8aa5a23b1634ab48f66b4dd8845f4f976a
gheath340 <heathsmail0@gmail.com>
2023-05-20 10:44:17 -0600

    found and fixed small error

--- client/src/components/ToDoItem.jsx
+++ client/src/components/ToDoItem.jsx
@@ -1,8 +1,8 @@
 export function TodoItem({ complete, id, text, toggleTodo, deleteTodo }) {
     return (
         <li className="flex justify-evenly gap-3 bg-sky-900 w-full py-1 px-4 rounded-xl">
-            <input id="toggle" type="checkbox" checked={complete} onChange={e => toggleTodo(id)} className="accent-sky-950 hover:accent-white"/>
-            <label htmlFor="toggle" className="flex cursor-pointer select-none items-center">{text}</label>
+            <input type="checkbox" checked={complete} onChange={e => toggleTodo(id)} className="accent-sky-950 hover:accent-white"/>
+            <label className="flex cursor-pointer select-none items-center">{text}</label>
             <button onClick={() => deleteTodo(id)} className="border rounded-md hover:bg-white hover:text-sky-950 hover:duration-200 px-2">
             Delete
             </button>
